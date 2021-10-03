5ecb6f052e27f065f307096bbeff264218ce975c
mzvezdeva <mzvezdeva@gmail.com>
2021-10-02 20:08:08 -0500

    Add Browser Commands and Navigation within website

--- src/BrowserCommand/BrowserCommand.java
+++ src/BrowserCommand/BrowserCommand.java
@@ -1,2 +1,31 @@
-package BrowserCommand;public class BrowserCommand {
+package BrowserCommand;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserCommand {
+
+    static WebDriver driver; //when we are building framework we will do one reference variable to reuse it, so we will make it static but make sure you always initialize it before you want to use it
+
+    public static void main(String[] args) {
+        String dir = System.getProperty("user.dir"); //we are doing it on that way in case someone downloads our project and work on it and our paths will be different but on that way is going to work for everyone
+        System.setProperty("webdriver.chrome.driver", dir + "//drivers//chromedriver.exe");
+        driver = new ChromeDriver();
+        //Navigate to facebook.com
+        driver.get("https://www.facebook.com");
+        //getting title of the web page
+        String pageTitle = driver.getTitle();
+        System.out.println("Page Title: " + pageTitle);
+        //get the current url of the page where the driver is at to verify is it the current url
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("Current URL: " + currentUrl);
+        //get page source will return the whole page source code
+        String pageSource = driver.getPageSource();
+        System.out.println("Page source: " + pageSource);
+        //close the browser of the current session
+        driver.close();
+        //this will close all the browsers open by the driver, it`s like terminating command to terminate the object and is better to use quit instead of close, just in case
+        //for example when you are testing and in the session of testing you click on a path that open another page but actually is using the same driver, so with quit you will close all of them
+        driver.quit();
+    }
 }
--- src/MyFirstSeleniumTest/MyFirstSeleniumTest.java
+++ src/MyFirstSeleniumTest/MyFirstSeleniumTest.java
@@ -1,15 +1,30 @@
 package MyFirstSeleniumTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MyFirstSeleniumTest {
 
     public static void main(String[] args) {
 
+        String dir = System.getProperty("user.dir");  //value of the directory that you are currently in,this will return the path for that project
+        System.out.println(dir);
 
+        System.setProperty("webdriver.chrome.driver", dir + "//drivers//chromedriver.exe");
 
+        WebDriver driver = new ChromeDriver();
 
+        driver.get("https://facebook.com");
 
+        String pageTitle = driver.getTitle();
 
-
+        if (!pageTitle.equalsIgnoreCase("Facebook - Log In or Sign Up")) {
+            System.out.println("You are at the wrong page!");
+            driver.close();
+        } else {
+            System.out.println("Page title: " + pageTitle);
+            driver.close();
+        }
 
     }
 }
